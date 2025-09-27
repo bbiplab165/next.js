fd7929ae6ee5ad241324972b102576c994d3310a
Biplab Biswas <bbiplab165@gmail.com>
2025-09-27 22:24:23 +0530

    Initial commit

---
+++ src/app/products/[id]/page.js
@@ -0,0 +1,12 @@
+
+
+
+export default async function Products({params}) {
+
+    const {id}= await params
+  return (
+  <div>
+    <h4>Product {id} details page — content coming soon!</h4>
+  </div>
+  );
+}
